ca9036c762556068fdc93293fd6576f5b597639f
nikitos123321 <nikitasaronov1907@gmail.com>
2024-09-04 17:16:13 +0300

    Практика 1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+alert("Я JavaScript");
+let surname = prompt("Ваша фамилия?");
+let name = prompt("Ваше имя?");
+let otchestvo = prompt("Ваше отчество?");
+alert(`Ваша фамилия: ${surname}, Ваше имя: ${name}, Ваше отчество: ${otchestvo}`);
+let isTrue = confirm(`Верны ли утверждения? Ваша фамилия: ${surname}, Ваше имя: ${name}, Ваше отчество: ${otchestvo}`);
+
